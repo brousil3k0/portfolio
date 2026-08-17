60182b99f42f7e675db784fd89d8659294f3daee
ondrejbrousil <ondrej@brousil3k0.dev>
2026-08-17 23:42:26 +0200

    Vetsi posun radku slogan doleva/doprava

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -85,7 +85,7 @@ export function Hero({ lang }: { lang: Lang }) {
                   enough to read as an intentional off-center composition,
                   not a layout bug. Pure transform (no margin) so it doesn't
                   disturb the block's own centered width. */}
-              <span className="block -translate-x-3 whitespace-nowrap sm:-translate-x-4 md:-translate-x-6 lg:-translate-x-8">
+              <span className="block -translate-x-6 whitespace-nowrap sm:-translate-x-8 md:-translate-x-12 lg:-translate-x-16">
                 {t.hero.sloganLine1.map((seg, i) => (
                   <span
                     key={i}
@@ -96,7 +96,7 @@ export function Hero({ lang }: { lang: Lang }) {
                   </span>
                 ))}
               </span>
-              <span className="block translate-x-3 whitespace-nowrap sm:translate-x-4 md:translate-x-6 lg:translate-x-8">
+              <span className="block translate-x-6 whitespace-nowrap sm:translate-x-8 md:translate-x-12 lg:translate-x-16">
                 {t.hero.sloganLine2.map((seg, i) => (
                   <span
                     key={i}
